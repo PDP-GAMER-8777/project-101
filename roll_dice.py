8b880f03d11a5dd8bb199f3b7f8a668dd3ffeffc
PDP-GAMER-8777 <84278882+PDP-GAMER-8777@users.noreply.github.com>
2022-12-26 22:35:06 -0800

    Add files via upload

---
+++ roll_dice.py
@@ -0,0 +1,46 @@
+import random
+
+while response == "y" :  
+
+  no=random.randint(1,6)
+
+  if no==1:
+   print("[----]") 
+   print("[    ]") 
+   print("[ 0  ]") 
+   print("[    ]")
+   print("[----]")
+  if no==2:
+   print("[----]") 
+   print("[    ]") 
+   print("[0   ]") 
+   print("[   0]")
+   print("[----]")   
+  if no==3:
+   print("[----]") 
+   print("[0   ]") 
+   print("[ 0  ]") 
+   print("[   0]")
+   print("[----]")      
+  if no==3:
+   print("[----]") 
+   print("[0 0 ]") 
+   print("[0 0 ]") 
+   print("[    ]")
+   print("[----]") 
+  if no==5:
+   print("[----]") 
+   print("[0  0]") 
+   print("[  0 ]") 
+   print("[0  0]")
+   print("[----]")     
+  if no==5:
+   print("[----]") 
+   print("[0  0]") 
+   print("[0  0]") 
+   print("[0  0]")
+   print("[----]")      
+
+print("enter y to roll dice again" "enter n to dont roll dice again" )     
+
+resonse=str("enter a respone to roll dice")
